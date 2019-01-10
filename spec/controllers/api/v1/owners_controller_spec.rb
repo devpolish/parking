9de057066ab0af23482a78822146f0e949313309
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-10 16:56:43 -0500

    add more cases

--- spec/controllers/api/v1/owners_controller_spec.rb
+++ spec/controllers/api/v1/owners_controller_spec.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
 describe Api::V1::OwnersController, type: :controller do
-  # Fixtures
-  let(:payload) { { owner: { name: 'Nardo Nykolyszyn', document_type: 'CE', document: 4874435534} }}
-  let(:payload_missing_document_type) { { owner: { name: 'Nardo Nykolyszyn', document_type: 43, document: 4874435534} }}
+  # Fixtures || (Replace values using require_fixture helper from fixtures folder)
+  let(:payload) { { owner: { name: 'Nardo Nykolyszyn', document_type: 'CE', document: 4874435534 } }}
+  let(:payload_missing_document_type) { { owner: { name: 'Nardo Nykolyszyn', document_type: 43, document: 4874435534 } }}
   let(:payload_missing_name) { { owner: { document_type: 'CE', document: 4874435534} }}
-
-  context 'errors testing' do
+  let(:payload_with_different_atom) { { unknown: { name: 'Nardo Nykolyszyn', document_type: 'CE', document: 4874435534 } }}
+  
+  context 'API errors testing' do
     it 'should an unprocessable entity when payload is empty' do
       post :create
       expect(response.status).to eq(422)
     end
 
-    it 'expect something' do
+    it 'expect to throw an error when atom attribute is different' do
+      post :create, params: payload_with_different_atom
+      expect(response.body).to include_json(
+        errors: 'param is missing or the value is empty: owner'
+      )
+    end
+  end
+
+  context 'entity errors testing' do
+    it 'expect to get 200 code when owner is created' do
       post :create, params: payload
       expect(response.status).to eq(200)
     end
